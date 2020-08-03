fabb1cacfd94a8ca2ba0137eeb8b78c04a91ab52
Hasan Saleem <55816213+hasan-umanitoba@users.noreply.github.com>
2020-08-03 07:30:41 -0500

    Setting up

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="grid-container">
+      <header>
+        <a href="/" >React Shopping Cart</a>
       </header>
+      <main>
+        Product Listings
+      </main>
+      <footer>
+This is a footer
+      </footer>
     </div>
   );
 }
